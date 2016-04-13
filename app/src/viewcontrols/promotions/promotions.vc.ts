969221b2ff3b3995d6c9cbe7905ca203a4746731
Will Kirkpatrick <willkirkpatrick@gmail.com>
2016-04-13 16:18:00 -0500

    centered text on collapse, added modal to register button, began styling modal

--- app/src/viewcontrols/promotions/promotions.vc.ts
+++ app/src/viewcontrols/promotions/promotions.vc.ts
@@ -4,7 +4,19 @@ import BaseViewControl from '../base/base.vc';
 export default class PromotionsViewControl extends BaseViewControl {
     templateString: string = require('./promotions.vc.html');
 
-    context: any = {};
+    context = {
+        showRegModal: false
+    };
+    
+    // Here, you need to toggle the boolean. From false to true, true to false.
+    toggleModal() {
+        if (this.context.showRegModal === false) {
+            this.context.showRegModal = true;
+        } else {
+            this.context.showRegModal = false;
+        }
+    }
+    
 }
 
 register.viewControl('promotions-vc', PromotionsViewControl);
