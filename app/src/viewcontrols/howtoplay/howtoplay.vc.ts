90b4f95128c110b45490aa4de322993d6861a73f
Jesla <jesslar415@gmail.com>
2016-04-13 20:40:23 -0500

    changes

--- app/src/viewcontrols/howtoplay/howtoplay.vc.ts
+++ app/src/viewcontrols/howtoplay/howtoplay.vc.ts
@@ -1,12 +1,27 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
 
+
 export default class HowtoplayViewControl extends BaseViewControl {
     templateString: string = require('./howtoplay.vc.html');
 
-    context: any = {};
+    context = {
+        showVideoTab: false
+    };
+    
+    toggleTab() {
+        console.log("it's working!")
+        this.context.showVideoTab = true;
+        
+        //click event with mouse click, deativates on click release
+        // document.getElementById(...)
+        // active-started-tab
+        // active-videos-tab 
+    };
+    
+    
+    
 }
-
 // if (this.context.toggleTab){
 // this.context.toggleTab = true,
 
