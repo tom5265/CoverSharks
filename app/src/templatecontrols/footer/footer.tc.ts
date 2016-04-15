39c32a9241d38d97aec43348b1a26bd5a383c800
tom5265 <tom5265@gmail.com>
2016-04-15 13:34:01 -0500

    fixed footer scrolling problem with index.html, added scrolltop function to base.ts

--- app/src/templatecontrols/footer/footer.tc.ts
+++ app/src/templatecontrols/footer/footer.tc.ts
@@ -1,7 +1,18 @@
 import {register, ui} from 'platypus';
+import HomeViewControl from '../../viewcontrols/home/home.vc'
+import AboutViewControl from '../../viewcontrols/about/about.vc';
+import HowToPlayViewControl from '../../viewcontrols/howtoplay/howtoplay.vc';
+import PromotionsViewControl from '../../viewcontrols/promotions/promotions.vc';
 
 export default class FooterTemplateControl extends ui.TemplateControl {
     templateString: string = require('./footer.tc.html');
+    context: any = {
+        home: HomeViewControl,
+        about: AboutViewControl,
+        howtoplay: HowToPlayViewControl,
+        promotions: PromotionsViewControl
+        
+    }
 }
 
 register.control('footer', FooterTemplateControl);
